9f866c8005c804e07cebcb5f15117fdd7e43e273
FatmaYahya01 <fatmayehya01@gmail.com>
2022-01-25 08:00:17 +0200

    HTML& CSS & JS project

---
+++ phone.js
@@ -0,0 +1,14 @@
+var btnvar1 = document.getElementById("btnh");
+
+function Toggle1(id) {
+     var btnvar1  =document.getElementById(id)
+    if (btnvar1.style.color == "red") {
+        btnvar1.style.color = "grey"
+    } else {
+        btnvar1.style.color = "red"
+    
+    }
+}
+
+
+
